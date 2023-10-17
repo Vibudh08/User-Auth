107c3d26abfc8ca99dace5ff877cc9f7b5d1c47a
Vibudh08 <vibudhrathore8@gmail.com>
2023-10-17 13:51:26 +0530

    error page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,56 +1,50 @@
-const userModel = require('../model/User')
+const userModel = require("../model/User");
 class userController {
+  static home = (req, res) => {
+    res.render("index");
+  };
 
-    static home = (req,res)=>{
-        res.render('index')
-    }
+  static login = (req, res) => {
+    res.render("login");
+  };
 
-    static login = (req,res)=>{
-        res.render('login')
-    }
-    
-    static registration = (req,res)=>{
-        res.render('registration')
-    }
+  static registration = (req, res) => {
+    res.render("registration");
+  };
 
-    static createDoc = async(req,res)=>{
-        try {
-            const doc = new userModel({
-                name : req.body.name,
-                email : req.body.email,
-                password : req.body.password
-            })
-            await doc.save()
-            res.redirect('/login')
-        } catch (error) {
-            console.log(error)
+  static createDoc = async (req, res) => {
+    try {
+      const doc = new userModel({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      await doc.save();
+      res.redirect("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static validateLogin = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const result = await userModel.findOne({ email: email });
+      // console.log(result)
+
+      if (result != null) {
+        if (result.email == email && result.password == password) {
+          res.send("<h1>Login Successfully</h1>");
+        } else {
+          res.render("Error1");
         }
+      } else {
+        res.render("Error2");
+      }
+    } catch (error) {
+      console.log(error);
     }
-
-    static validateLogin = async(req,res)=>{
-        try {
-            const {email , password} = req.body
-            const result = await userModel.findOne({email:email})
-            // console.log(result)
-
-            if(result != null){
-                if(result.email == email && result.password == password){
-                    res.send('<h1>Login Successfully</h1>')
-                }
-                else{
-                    res.send('<h1>Password is not correct</h1>')
-                }
-            }
-            else{
-                res.send('<h1>User is not registered</h1>')
-            }
-
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-
+  };
 }
 
-module.exports = userController
+module.exports = userController;
